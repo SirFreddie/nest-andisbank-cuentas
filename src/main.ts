130c722b0ff539f179bf467e3ae8b6965207cd8e
Rodrigo Luque <rodrigoluquepuig@gmail.com>
2024-09-04 20:35:33 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ANDISBANK - CUENTAS')
+    .setDescription('API para el manejo de cuentas bancarias')
+    .setVersion('1.0')
+    .addTag('cuentas')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
